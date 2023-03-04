3a4bc630c55e6bf32595c9439de957ac9015ccf5
Deren Teo <teo.deren@icloud.com>
2023-03-04 18:16:35 +1000

    Improved street sign segmentation
    Combined red channel and saturation channel as first step of image
    processing to reduce intensity of saturated but non-reddish
    (incl. non-yellow) background.

--- a1/src/road_sign_segmentation.py
+++ a1/src/road_sign_segmentation.py
@@ -8,59 +8,32 @@ from pathlib import Path
 A1_ROOT = Path(__file__).parent.parent.resolve()
 
 
-def process_image(img, thresh=0):
+def process_image(img, alpha=0.4, beta=0.6, d=15, sigmaColor=25, sigmaSpace=75, thresh=180):
     '''
-    Attempt to process an image containing one or more street signs into a
-    binary image, where the area of any street signs is white, and the
-    remaining area is black.
+    TODO: documentation
     '''
-    
+    img_red = img[:, :, 2]
+
     # Yellow signs tend to have high saturation relative to background
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     img_sat = img_hsv[:, :, 1]
 
-    # Blur saturation image
-    # img_blurred = cv.blur(img_sat, ksize=(7, 7))
-    # img_blurred = cv.GaussianBlur(img_sat, ksize=(11, 11), sigmaX=0)
-    # img_blurred = cv.medianBlur(img_sat, ksize=7)
-    img_blurred = cv.bilateralFilter(img_sat, d=9, sigmaColor=75, sigmaSpace=75)
+    # Combine red channel and saturation channel to reduce intensity of 
+    # saturated but non-reddish (incl. non-yellow) background
+    img_prod = cv.addWeighted(img_red, alpha, img_sat, beta, 0)
+
+    # Blur saturation image using bilateral filter to preserve edges
+    img_blurred = cv.bilateralFilter(img_prod, d, sigmaColor, sigmaSpace)
 
     # Binarize saturation image
     maxval = np.amax(img_blurred)
     _, img_binarized = cv.threshold(img_blurred, thresh, maxval, cv.THRESH_BINARY)
 
-    # img_blurred = cv.blur(img_binarized, ksize=(7, 7))
-
-    kernel = np.ones((3, 3), np.uint8)
-    img_opened = cv.morphologyEx(img_binarized, cv.MORPH_OPEN, kernel)
-
-    return img_opened
-
-# def segment_image(img):
-#     '''TODO: documentation'''
-
-#     kernel = np.ones((71, 71), np.uint8)
-#     sure_bg = cv.dilate(img, kernel)
-
-#     kernel = np.ones((71, 71), np.uint8)
-#     sure_fg = cv.erode(img, kernel)
-
-#     sure_fg = np.uint8(sure_fg)
-#     unknown = cv.subtract(sure_bg, sure_fg)
-
-#     _, markers = cv.connectedComponents(sure_fg)
-#     markers = markers + 1
-#     markers[unknown==255] = 0
-
-#     img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-#     markers = cv.watershed(img, markers)
-#     img[markers==-1] = [255, 0, 0]
-
-#     return markers
+    return img_binarized
 
 
 def bounding_rect(img, dst):
-    ''''''
+    '''TODO: documentation'''
     x, y, w, h = cv.boundingRect(img)
     cv.rectangle(dst, (x, y), (x + w, y + h), (255, 0, 0), 1)
     return dst
@@ -69,13 +42,21 @@ def bounding_rect(img, dst):
 def main():
     '''TODO: documentation'''
 
-    img_fp = Path(A1_ROOT, 'data', 'street_signs', 'images0.jpg')
-    img_raw = cv.imread(str(img_fp))
-    img_rgb = cv.cvtColor(img_raw, cv.COLOR_BGR2RGB)
-    img_processed = process_image(img_raw, thresh=200)
-    # img_segmented = bounding_rect(img_processed, img_rgb)
+    fig, axs = plt.subplots(3, 4)
+    
+    for i, ax in enumerate(axs.flat):
+        ax.axis('off')
+        if i < 11:
+            img_fp = Path(A1_ROOT, 'data', 'street_signs', f'images{i}.jpg')
 
-    plt.imshow(img_processed, cmap='gray')
+            img_raw = cv.imread(str(img_fp))
+            img_rgb = cv.cvtColor(img_raw, cv.COLOR_BGR2RGB)
+
+            img_processed = process_image(img_raw)
+            img_segmented = bounding_rect(img_processed, img_rgb)
+
+            ax.imshow(img_segmented, cmap='gray')
+    
     plt.show()
 
 
