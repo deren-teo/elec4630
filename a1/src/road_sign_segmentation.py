4de155037e3db6040f4038e52a5309e1ddf9a527
Deren Teo <teo.deren@icloud.com>
2023-03-04 19:08:44 +1000

    Add documentation

--- a1/src/road_sign_segmentation.py
+++ a1/src/road_sign_segmentation.py
@@ -10,7 +10,26 @@ A1_ROOT = Path(__file__).parent.parent.resolve()
 
 def process_image(img, alpha=0.4, beta=0.6, d=15, sigmaColor=25, sigmaSpace=75, thresh=185):
     '''
-    TODO: documentation
+    Processes an image to remove all image details except street signs.
+    This is done by taking the red channel and saturation channel of img,
+    then blurring and binarizing the resulting image.
+
+    The red channel and saturation channel are chosen as most street signs
+    have relatively high red intensity, such as those with yellow or red
+    features, and high saturation compared to the background.
+
+    Parameters:
+        img - image to process
+        alpha - fractional contribution of red channel
+        beta - fractional contribution of saturation channel
+        d - blurring kernel diameter (using a bilateral filter)
+        sigmaColor - blurring sigma for dissimilar colours
+        sigmaSpace - blurring sigma for dissimilar spatial regions
+        thresh - binarization threshold (out of 255)
+
+    Returns:
+        Black and white image with (ideally) most or all elements removed
+        from the original image except street signs.
     '''
     img_red = img[:, :, 2]
 
@@ -32,17 +51,31 @@ def process_image(img, alpha=0.4, beta=0.6, d=15, sigmaColor=25, sigmaSpace=75, 
     return img_binarized
 
 
-def bounding_rect(img, dst):
-    '''TODO: documentation'''
+def bounding_rect(img, dst, colour=(255, 0, 0), lw=1):
+    '''
+    Draws a bounding rectangle around all white points in the provided image.
+
+    Parameters:
+        img - image used to find bounding box; assumed to be single-channel
+        dst - image that bounding box is applied to; assumed to be RGB
+        colour - colour of bounding box (default red)
+        lw - width of bounding box in pixels
+
+    Returns:
+        Image dst overlayed with a rectangular bounding box over all white
+        ares in image img.
+    '''
     x, y, w, h = cv.boundingRect(img)
-    cv.rectangle(dst, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    cv.rectangle(dst, (x, y), (x + w, y + h), colour, lw)
     return dst
 
 
 def main():
-    '''TODO: documentation'''
-
-    fig, axs = plt.subplots(3, 4)
+    '''
+    Visualise all 11 street sign samples with segmentation algorithm applied.
+    Identified street signs are marked inside red rectangles.
+    '''
+    _, axs = plt.subplots(3, 4)
     
     for i, ax in enumerate(axs.flat):
         ax.axis('off')
