2a447c8a4ed25934459d895c9f0453c9e98bcde2
Deren Teo <teo.deren@icloud.com>
2023-03-17 21:10:12 +1000

    A1Q3: vomitted a road edge detector

--- a1/src/hough_transforms.py
+++ a1/src/hough_transforms.py
@@ -31,14 +31,70 @@ def contour_aspect(contour: np.ndarray) -> float:
     return float(w) / h
 
 def find_road_edges(img: Image) -> List[Line]:
-    ''''''
+    '''
+    Detect the position of the road edge in the provided image. It is expected
+    that the road edge will be segmented by the car in the foreground.
+
+    Returns a list of lines (defined between two Cartesian points) indicating
+    the sections of identified road edge.
+    '''
+
+    # Convert the image to HSV and extract the saturation channel, which
+    # contrasts the road well against the grass
+    img = cv.cvtColor(img, cv.COLOR_BGR2HSV)[:, :, 1]
+
+    # Binarize the frame to enhance the road
+    img = cv.threshold(img, thresh=100, maxval=255, type=cv.THRESH_BINARY)[1]
+
+    # Morphologically close the image using a very large (gigantic) kernel to
+    # absolutely eradicate every last detail except the road edge border
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel=np.ones((121, 121)))
+    img = np.invert(img)
+
+    # Split the image into left and right halves to identify road extent
+    _, w = img.shape
+    img_l = img[:, :int(w / 2)]
+    img_r = img[:, int(w / 2):]
+
+    # For each half, creating a bounding rectangle defining the road extent
+    _, _, extent_l, _ = cv.boundingRect(img_l)
+    _, _, extent_r, _ = cv.boundingRect(img_r)
+
+    # Run edge detection to identify the road edge border
+    img = cv.Canny(img_l, 100, 200)
+
+    # Apply the Hough line transform to identify the road edge
+    # Threshold should be large enough to ignore noise
+    line = cv.HoughLines(img, rho=1, theta=(np.pi / 180), threshold=100)[-1]
+
+    # Calculate the equation of the line formed by rho and theta
+    rho, theta = line[0]
+
+    # Use the equation of a line to calculate the section of the road between
+    # the left and right borders and extents
+    a = np.cos(theta)
+    b = np.sin(theta)
+    m = -a / b
+    c = rho * (b + a**2 / b)
+
+    # Define the lines of the road edge, not overlapping the foreground
+    edge_l = (0, int(c), extent_l, int(m * extent_l + c))
+    edge_r = (w - extent_r, int(m * (w - extent_r) + c), w, int(m * w + c))
+
+    return [edge_l, edge_r]
 
 def find_broomstick(img: Image) -> List[Line]:
-    ''''''
+    '''
+    Detect the position of the broomstick in the provided image.
+
+    Returns a list of one line (defined between two Cartesian points)
+    indicating the position of the broomstick.
+    '''
 
 def find_wheelhubs(img: Image) -> List[Circle]:
     '''
     Detect the position and size of the wheel hubs in the provided image.
+
     Returns a list of circles (defined by a Cartesian centre and radius)
     indicating the outline of the identified wheel hubs.
     '''
